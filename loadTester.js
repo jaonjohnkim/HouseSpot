de6c57c7030ffce8bb7947c8c5895cb9abd696cf
Ja-on John Kim <jaonjohnkim@github.com>
2017-11-01 16:57:33 -0700

    load tester

---
+++ loadTester.js
@@ -0,0 +1,40 @@
+const request = require('request-promise');
+
+setInterval(() => {
+  const zipcodes = [
+    94102,
+    94103,
+    94104,
+    94105,
+    94107,
+    94108,
+    94109,
+    94110,
+    94111,
+    94112,
+    94114,
+    94115,
+    94116,
+    94117,
+    94118,
+    94121,
+    94122,
+    94123,
+    94124,
+    94127,
+    94129,
+    94130,
+    94131,
+    94132,
+    94133,
+    94134,
+    94158
+  ];
+  const granularity = ['day', 'week', 'month'];
+
+  let zipcode = zipcodes[Math.round(Math.random() * (zipcodes.length - 1))];
+  let gran = granularity[Math.round(Math.random() * (granularity.length - 1))];
+  // zipcode = 94111;
+  request(`https://housespot.herokuapp.com/json?zipcode=${zipcode}&startDate=2017-07-01T00:00:00.000&endDate=2017-10-25T00:00:00.000&granularity=${gran}`)
+  console.log('Pinged for zipcode:', zipcode);
+}, 10);
